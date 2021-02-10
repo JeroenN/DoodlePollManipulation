746a4ee8e5e28245604cdaddd821e086b08e5500
Isabelle <i.w.m.tilleman@student.rug.nl>
2021-02-10 11:31:03 +0100

    created function that creates agents, function that creates environment, and a function that prints both

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
-import enviroment
-import agent
+from environment import Environment
+from agent import Agent 
 
 # TODO:
-# Make it possible to create multiple agents
 # create something that counts the utility at the end of the round
 # make a standard strategy, this strategy should have a threshold and a way to determine when it should vote
 
+def create_agents(n_agents, environment):
+    agents = []
+    for i in range(n_agents):
+        agents.append(Agent(environment, i))
+    return agents
 
-def create_agents():
-    pass
+def create_enviroment(n_time_slots):
+    return Environment(n_time_slots)
 
-
-def create_enviroment():
-    pass
-
+def print_game(environment, agents):
+    print(environment, '\n')
+    print('\n'.join(map(str, agents)))
 
 def main():
-    create_agents()
-    create_enviroment()
-
+    environment = create_enviroment(int(input("How many dates are in the Doodle poll?: "))) # create and store environment
+    agents = create_agents(int(input("How many voters are in the Doodle poll?: ")), environment) # create and store agents
+    print_game(environment, agents)
 
 if __name__ == "__main__":
     main()
