e8a6344aaae2f5a52fadf927711e422f42036b92
Isabelle <i.w.m.tilleman@student.rug.nl>
2021-03-16 10:46:39 +0100

    (hopefully final) merge fix

--- quick_sort.py
+++ quick_sort.py
@@ -13,6 +13,7 @@ def partition(arr, idx, low, high):
     idx[i + 1], idx[high] = idx[high], idx[i + 1]
     return i + 1
 
+#sorts from low to high 
 def quick_sort(arr, idx, low, high):
     if low < high:
         pi = partition(arr, idx, low, high)
