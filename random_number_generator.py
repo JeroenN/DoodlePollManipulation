141905d02915603efdd105af57114b04f5e7c83e
unknown <jcaniemendal@gmail.com>
2021-04-02 16:32:31 +0200

    now properly calculates the normal distribution for popular prediction agent

--- random_number_generator.py
+++ random_number_generator.py
@@ -4,7 +4,6 @@ import numpy as np
 # has to be between a certain range
 def generate_random_number_normal_distribution(mean=0.5, standard_deviation=0.35, start_range=0, end_range=1):
     random_number = np.random.normal(mean, standard_deviation, 1)
-    while start_range > random_number > end_range:
+    while random_number < start_range or random_number > end_range:
         random_number = np.random.normal(mean, standard_deviation, 1)
-
-    return random_number
+    return random_number[0]
